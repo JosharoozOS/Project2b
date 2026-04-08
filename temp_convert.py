c0fb3a13a84f5df7dc72a93c9bab92419c1d6da7
JosharoozOS <heinjos@oregonstate.edu>
2026-04-08 09:59:44 -0700

    Add files via upload

---
+++ temp_convert.py
@@ -0,0 +1,10 @@
+# Author: Josh Hein
+# GitHub username: JosharoozOS
+# Date: 4/8/2026
+# Description: Create a system that converts Celsius into Fahrenheit
+
+celsius = float(input("Please enter a Celsius temperature: "))
+fehrenheit = ((9/5)*celsius + 32)
+
+print("The equivalent Fahrenheit temperature is:", round(fehrenheit,1))
+
