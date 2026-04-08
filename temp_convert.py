843d0dce0834b6a94023f8def0b10cb47d8b8412
JosharoozOS <heinjos@oregonstate.edu>
2026-04-08 10:05:16 -0700

    Improve input/output formatting in temperature converter
    Refactor input and output prompts for clarity.

--- temp_convert.py
+++ temp_convert.py
@@ -3,8 +3,9 @@
 # Date: 4/8/2026
 # Description: Create a system that converts Celsius into Fahrenheit
 
-celsius = float(input("Please enter a Celsius temperature: "))
+print("Please enter a Celsius temperature:")
+celsius = float(input())
 fehrenheit = ((9/5)*celsius + 32)
 
-print("The equivalent Fahrenheit temperature is:", round(fehrenheit,1))
-
+print("The equivalent Fahrenheit temperature is:")
+print(round(fehrenheit,1))
